830c294c6ea2ced4c62fc4f2536b1ffc66f026cb
gabrielmuller777 <gabriel.ceolato@yahoo.com.br>
2020-04-13 21:56:24 -0300

    delete

---
+++ src/components/Mainpage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <div class="head">
+      <h1>CARREGADORES <BR> AGRÍCOLAS</h1>
+      <h2>Simples para seu trator, essencial para seu trabalho.</h2>
+    </div>
+    <div class="photo"></div>
+    <div class="logo"></div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Mainpage'
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
