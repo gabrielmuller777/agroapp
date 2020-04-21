0e35942622a76d1a1f8ec2408551e422f6767f3b
gabrielmuller777 <gabriel.ceolato@yahoo.com.br>
2020-04-21 09:02:08 -0300

    1

---
+++ app/components/Home.vue
@@ -0,0 +1,40 @@
+<template>
+    <Page>
+        <ActionBar class="banana">
+            <Label text="Homeeeeee"></Label>
+        </ActionBar>
+
+        <GridLayout>
+            <Label class="info">
+                <FormattedString>
+                    <Span :text="message"/>
+                </FormattedString>
+            </Label>
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+
+var banene = []
+    export default {
+        computed: {
+            message() {
+                return "Banana";
+            }
+        }
+    };
+</script>
+
+<style scoped>
+
+    .info {
+        font-size: 20;
+        horizontal-align: center;
+        vertical-align: center;
+    }
+
+    .banana {
+        background-color: red;
+    }
+</style>
